a538cd7189357db2f0f101b0649080ea168eaa26
AndrewReis <andrew-reiis@outlook.com>
2020-08-21 10:29:55 -0300

    babel configurado

--- src/index.js
+++ src/index.js
@@ -7,3 +7,7 @@
 const soma = (a,b) => a + b ;
 
 console.log(soma(1,3));
+
+
+//Apos rodar "yarn babel src/index.js --out-file public/bundle.js " no terminal, será criado um novo arquivo na pasta "public" chamado "bundle.js" 
+//Note que este arquivo foi preenchido automaticamente, com o mesmo código que escrevemos, só que com a sintaxe de função tradicional.
